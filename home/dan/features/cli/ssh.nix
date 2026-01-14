a06871b3ed4875a7b5708d0b6bcfbecaf3580c60
Daniel Kressner <989603+kressnerd@users.noreply.github.com>
2026-01-14 09:30:04 +0100

    feat: update SSH identity files

--- home/dan/features/cli/ssh.nix
+++ home/dan/features/cli/ssh.nix
@@ -15,28 +15,28 @@
       "github-personal" = {
         hostname = "github.com";
         user = "git";
-        identityFile = "~/.ssh/id_ed25519";
+        identityFile = "~/.ssh/id_ed25519_personal_2025-06-18";
         identitiesOnly = true;
       };
 
       "github-company" = {
         hostname = "github.com";
         user = "git";
-        identityFile = "~/.ssh/id_ed25519";
+        identityFile = "~/.ssh/id_ed25519_company_2025-06-18";
         identitiesOnly = true;
       };
 
       "github-client001" = {
         hostname = "github.com";
         user = "git";
-        identityFile = "~/.ssh/id_ed25519_2025-07-22-temp";
+        identityFile = "~/.ssh/id_ed25519_client001_2025-07-22";
         identitiesOnly = true;
       };
 
       "bitbucket-client002" = {
         hostname = "bitbucket.org";
         user = "git";
-        identityFile = "~/.ssh/id_ed25519";
+        identityFile = "~/.ssh/id_ed25519_client002_2026-01-13";
         identitiesOnly = true;
       };
     };
