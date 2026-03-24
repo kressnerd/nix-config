b810b1413c4b435db8566c26bacfda66e8e7f2f5
Daniel Kressner <989603+kressnerd@users.noreply.github.com>
2026-03-24 19:33:50 +0100

    feat: add host template

---
+++ templates/host/default.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../../hosts/common/global
+    ../../hosts/common/users/dan.nix
+    ./hardware.nix
+  ];
+
+  networking.hostName = "CHANGEME";
+  system.stateVersion = "25.11";
+}
