9c9b7b82483911f3fb2bc9c1f4fc1efad8b1d797
Daniel Kressner <989603+kressnerd@users.noreply.github.com>
2026-04-09 22:29:07 +0200

    fix(greetd): revert session path to hyprland package store path

--- hosts/thiniel/default.nix
+++ hosts/thiniel/default.nix
@@ -233,7 +233,7 @@
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --sessions /run/current-system/sw/share/wayland-sessions";
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --sessions ${pkgs-unstable.hyprland}/share/wayland-sessions";
           user = "greeter";
         };
       };
