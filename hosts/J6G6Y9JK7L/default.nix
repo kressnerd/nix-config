aad125e6bc935ad085c703592017fc7f5b5d8c49
Daniel Kressner <989603+kressnerd@users.noreply.github.com>
2026-06-04 16:11:14 +0200

    feat(stylix): update base16 color scheme for One Light theme

--- hosts/J6G6Y9JK7L/default.nix
+++ hosts/J6G6Y9JK7L/default.nix
@@ -113,7 +113,26 @@
     enable = true;
     autoEnable = false;
     polarity = "light";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/one-light.yaml";
+    base16Scheme = {
+      scheme = "One Light";
+      author = "Daniel Pfeifer (http://github.com/purpleKarrot)";
+      base00 = "fafafa";
+      base01 = "f0f0f1";
+      base02 = "e5e5e6";
+      base03 = "a0a1a7";
+      base04 = "696c77";
+      base05 = "383a42";
+      base06 = "202227";
+      base07 = "090a0b";
+      base08 = "ca1243";
+      base09 = "d75f00";
+      base0A = "c18401";
+      base0B = "50a14f";
+      base0C = "0184bc";
+      base0D = "4078f2";
+      base0E = "a626a4";
+      base0F = "986801";
+    };
     image = pkgs.runCommand "one-light-wallpaper.png" { nativeBuildInputs = [ pkgs.imagemagick ]; } ''
       magick -size 1920x1080 xc:#fafafa $out
     '';
